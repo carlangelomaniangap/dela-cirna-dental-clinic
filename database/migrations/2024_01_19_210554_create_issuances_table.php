6c1c3b305debeb875d22f8733b6695b40cce0ba7
carlangelomaniangap <carlangelomaniangap@gmail.com>
2024-12-12 12:24:11 +0800

    Issuances

---
+++ database/migrations/2024_01_19_210554_create_issuances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('issuances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('inventory_id');
+            $table->unsignedBigInteger('users_id');
+            $table->string('distribute_to');
+            $table->integer('issuance');
+            $table->timestamps();
+
+            $table->foreign('inventory_id')->references('id')->on('inventories')->onDelete('cascade');
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('issuances');
+    }
+};
